d16a257844cab2db4f405ea9288d935395dc4093
Grant Viklund <grant.viklund@gmail.com>
2019-08-07 11:57:04 -0700

    urls.py cleanup

--- helpme/urls.py
+++ helpme/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 
 from helpme import views
 
-# Paths for managing support requests go here...
 urlpatterns = [
-    # path("", views.HelpMeDashboardView.as_view(), name="helpme-dashboard"),
     path("request/", views.SupportRequestView.as_view(), name="helpme-submit-request"),
     path("success/", views.SupportRequestSuccessView.as_view(), name="helpme-success"),
+    # Paths for managing support requests go here...
 ]
-
