f5c8bfdc4fe73263ab29d25626376f0bcd3241bc
mgcamisa <mgcamisa@gmail.com>
2020-08-18 10:35:04 -0700

    initial implementation of dashboard and ticket detail views

--- helpme/urls.py
+++ helpme/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from helpme import views
 
 urlpatterns = [
+    path("", views.SupportDashboardView.as_view(), name="helpme-dashboard"),
     path("request/", views.SupportRequestView.as_view(), name="helpme-submit-request"),
     path("success/", views.SupportRequestSuccessView.as_view(), name="helpme-success"),
+    path("ticket/<str:uuid>/", views.TicketDetailView.as_view(), name="helpme-ticket-detail"),
     # Paths for managing support requests go here...
 ]
